36335470b2ff1e2b70b92966523c07525e402468
Corey Walker <74572313+Corey4005@users.noreply.github.com>
2022-12-02 14:54:49 +0000

    fixing github error

--- model/model_temps_with_input.py
+++ model/model_temps_with_input.py
@@ -1,1 +1,28 @@
-#!/usr/bin/env python3# -*- coding: utf-8 -*-"""Created on Tue Sep 13 14:31:59 2022This is a module to show how a text file containing solar and infrared valuescan be read in and processed to model temperatures across three atmosphericlayers. @author: coreywalker"""from modelEquations import model#textfile location containing EBM30 values model_params = './EBM30.txt'#instantiate model class objectobj = model()#get values from textfile and set them to class attributesobj.setVariables(model_params)#solve matrix equationsobj.calculateRadiationBudget()#solve the set of linear equations and print tempsobj.getTemperatures(HF=True, Params='EBM30') #print temperature parameters
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 13 14:31:59 2022
+
+This is a module to show how a text file containing solar and infrared values
+can be read in and processed to model temperatures across three atmospheric
+layers. 
+
+@author: coreywalker
+"""
+
+from modelEquations import model
+
+#textfile location containing EBM30 values 
+model_params = './EBM30.txt'
+
+#instantiate model class object
+obj = model()
+
+#get values from textfile and set them to class attributes
+obj.setVariables(model_params)
+
+#solve matrix equations
+obj.calculateRadiationBudget()
+
+#solve the set of linear equations and print temps
+obj.getTemperatures(HF=True, Params='EBM30') #print temperature parameters
